fc8c5c5161695e32d25f611a2986cf6ce6b6d8f3
oncletom <hi@oncletom.io>
2014-11-18 08:56:24 +0100

    Better cli.

--- bin/cli.js
+++ bin/cli.js
@@ -3,10 +3,26 @@
 'use strict';
 
 var inliner = require('../index.js');
-var argv = require('yargs').argv;
+var pkg = require('../package.json');
+var fs = require('fs');
+var join = require('path').join;
+var cwd = process.cwd();
 
-var inputStream = process.stdin;
-var outputStream = process.stdout;
+var argv = require('yargs')
+  .version(pkg.version, 'version')
+  .help('help')
+  .options('output', {
+    alias: 'o',
+    description: 'Destination filepath (default is stdout)'
+  })
+  .usage('$0 inputFile.html -o outputFile.html')
+  .example('cat inputFile.html | $0 -o outputFile.html', 'Piping in, writing out.')
+  .example('$0 inputFile.html | htmlhint', 'Reading it, piping out')
+  .strict()
+  .argv;
+
+var inputStream = argv._.length ? fs.createReadStream(join(cwd, argv._[0])) : process.stdin;
+var outputStream = argv.output ? fs.createWriteStream(join(cwd, argv.output)) : process.stdout;
 
 inputStream
   .pipe(inliner())
